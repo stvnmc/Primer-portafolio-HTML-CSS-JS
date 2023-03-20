af2070502107c5affa5433651a289d604cdc8761
stvnmc <stvnmc123@gmail.com>
2023-03-19 23:19:30 -0600

    terminado el proyecto, mejorando la funcinalidad de la pagina

--- script.js
+++ script.js
@@ -1,72 +1,69 @@
+const inferior = document.querySelector('.parteInferior')
 
-const parteS = document.querySelector(".parteSuperior")
-const menu = document.querySelector(".menu")
-const wrapper = document.querySelector(".informacionDeIconos")
-const menuIteam = document.querySelectorAll(".menuIteam")
-const portadaDeP = document.querySelector(".portadaDeP")
-const parteInferior = document.querySelector(".parteInferior")
-const cajaProyectos = document.querySelectorAll(".cajasDeProyectos")
-const elementos1 = document.querySelector(".primerContenido")
-const elementos2 = document.querySelector(".segundoContenido")
-const elementos3 = document.querySelector(".tercerContenido")
+//Redes sociales
 
+const Deslizantes = document.querySelectorAll(".carpetaDeslizantes")
+const iconosDeMenu = document.querySelectorAll(".menuIteam")
 
-
-
-menu.addEventListener("click", () => {
-    parteS.classList.toggle("open")
-    parteInferior.classList.toggle("bajo")
-});
-portadaDeP.addEventListener("click", () => {
-    parteInferior.classList.toggle("open")
-    // orisontal.parentNode.classList.remove('open')
-});
-
-
-
-function mover1() {
-    elementos1.style.transform = "translate(0%, -2%)";
-    elementos2.style.transform = "translate(110%, -100%)";
-    elementos3.style.transform = "translate(110%, -200%";
-}
-function mover2() {
-    elementos1.style.transform = "translate(110%, 0%)";
-    elementos2.style.transform = "translate(0%, -100%)";
-    elementos3.style.transform = "translate(110%, -200%";
-}
-function mover3() {
-    elementos1.style.transform = "translate(117%, 0%)";
-    elementos2.style.transform = "translate(106%, -100%)";
-    elementos3.style.transform = "translate(0%, -200%)";
-}
-
-
-// carucell
-menuIteam.forEach((item, index) => {
+iconosDeMenu.forEach((item, index) => {
     item.addEventListener("click", () => {
-        //chnge the current slide
+        Deslizantes[0].style.transform = `translateY(${-124 * index}%)`
+        Deslizantes[1].style.transform = `translateY(${-123 * index}%)`
+        Deslizantes[2].style.transform = `translateY(${-122 * index}%)`
+        Deslizantes[3].style.transform = `translateY(${-122 * index}%)`
 
-        wrapper.style.transform = `translateX(${-100 * index}vw)`;
 
+        inferior.classList.remove('proyecto')
+        inferior.classList.remove('info')
+        inferior.classList.add('arriba')
+
+
+        document.querySelector('.iconosDeMenu .activo').classList.remove('activo')
+        item.classList.toggle('activo')
     });
 });
 
 
 
+// Informacion
 
 const contenedor = document.querySelectorAll(".contenedor");
 const logosF = document.querySelectorAll('.frontal')
 
-
-
 logosF.forEach((item, index) => {
-    item.addEventListener("click", (e) => {
-        parteInferior.classList.remove('open')
-        contenedor[2].style.transform = `translateY(${-81 * index}vh)`
-        contenedor[0].style.transform = `translateY(${-82 * index}vh)`
-        contenedor[1].style.transform = `translateY(${-82 * index}vh)`
+    item.addEventListener("click", () => {
+        contenedor[0].style.transform = `translateY(${-109 * index}%)`
+        contenedor[1].style.transform = `translateY(${-109 * index}%)`
+        contenedor[2].style.transform = `translateY(${-109 * index}%)`
+
+
+        inferior.classList.remove('proyecto')
+        inferior.classList.remove('arriba')
+        inferior.classList.add('info')
+
 
         document.querySelector('.locosJBR .activo').classList.remove('activo')
+        item.classList.toggle('activo')
+    });
+});
+
+
+// Proyectos
+
+const proyectos = document.querySelectorAll(".proyectos")
+const numerosDePaginas = document.querySelectorAll('.numeros')
+
+numerosDePaginas.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        proyectos[0].style.transform = `translateY(${-69 * index}vh)`
+        proyectos[1].style.transform = `translateY(${-69 * index}vh)`
+        proyectos[2].style.transform = `translateY(${-69 * index}vh)`
+
+        inferior.classList.remove('arriba')
+        inferior.classList.remove('info')
+        inferior.classList.add('proyecto')
+
+        document.querySelector('.numerosDePaginas .activo').classList.remove('activo')
         item.classList.toggle('activo')
     });
 });
