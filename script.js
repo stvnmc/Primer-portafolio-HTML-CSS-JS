8b58641be7b6cd4dd692009c66051c00a2fc872e
stvnmc <stvnmc123@gmail.com>
2023-05-08 14:36:16 -0600

    1

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
 const inferior = document.querySelector('.parteInferior')
+//Redes sociales
+const Deslizantes = document.querySelectorAll(".carpetaDeslizantes")
+const iconosDeMenu = document.querySelectorAll(".menuIteam")
+//Principal
+const principal = document.querySelectorAll('.lista')
+// lenguages y franworks
+const contenedor = document.querySelectorAll(".contenedor")
+const logosF = document.querySelectorAll('.frontal')
+// Proyectos
+const proyectos = document.querySelectorAll(".proyectos")
+const numerosDePaginas = document.querySelectorAll('.numeros')
 
 
 //Principal
-const principal = document.querySelectorAll('.lista')
-
 principal.forEach((item, index) => {
     item.addEventListener("click", () => {
-
-
         if (index === 0) {
-            inferior.classList.remove('arriba')
-            inferior.classList.remove('info')
-            inferior.classList.add('proyecto')
-            item.classList.remove('.activo')
-
-        } else if (index === 1) {
-            inferior.classList.remove('proyecto')
             inferior.classList.add('arriba')
             inferior.classList.remove('info')
-        } else {
+            inferior.classList.remove('proyecto')
+        } else if (index === 1) {
             inferior.classList.remove('proyecto')
             inferior.classList.remove('arriba')
             inferior.classList.add('info')
+        } else {
+            inferior.classList.add('proyecto')
+            inferior.classList.remove('arriba')
+            inferior.classList.remove('info')
         }
         document.querySelector('.listas .activo').classList.remove('activo')
         item.classList.toggle('activo')
-
     })
 })
 
-
-
-
-
-
-
 //Redes sociales
-
-
-
-const Deslizantes = document.querySelectorAll(".carpetaDeslizantes")
-const iconosDeMenu = document.querySelectorAll(".menuIteam")
-
 iconosDeMenu.forEach((item, index) => {
     item.addEventListener("click", () => {
         Deslizantes[0].style.transform = `translateY(${-124 * index}%)`
@@ -49,50 +41,40 @@ iconosDeMenu.forEach((item, index) => {
         Deslizantes[2].style.transform = `translateY(${-122 * index}%)`
         Deslizantes[3].style.transform = `translateY(${-122 * index}%)`
 
-
         inferior.classList.remove('proyecto')
         inferior.classList.remove('info')
         inferior.classList.add('arriba')
 
-
         document.querySelector('.iconosDeMenu .activo').classList.remove('activo')
         item.classList.toggle('activo')
-        
 
+        document.querySelector('.listas .I').classList.add('activo')
+        document.querySelector('.listas .L').classList.remove('activo')
+        document.querySelector('.listas .P').classList.remove('activo')
     })
 })
 
-
-
-
-// Informacion
-
-const contenedor = document.querySelectorAll(".contenedor")
-const logosF = document.querySelectorAll('.frontal')
-
+// lenguages y franworks
 logosF.forEach((item, index) => {
     item.addEventListener("click", () => {
         contenedor[0].style.transform = `translateY(${-109 * index}%)`
         contenedor[1].style.transform = `translateY(${-109 * index}%)`
         contenedor[2].style.transform = `translateY(${-109 * index}%)`
 
-
         inferior.classList.remove('proyecto')
         inferior.classList.remove('arriba')
         inferior.classList.add('info')
 
-
         document.querySelector('.locosJBR .activo').classList.remove('activo')
         item.classList.toggle('activo')
+
+        document.querySelector('.listas .I').classList.remove('activo')
+        document.querySelector('.listas .L').classList.add('activo')
+        document.querySelector('.listas .P').classList.remove('activo')
     })
 })
 
-
 // Proyectos
-
-const proyectos = document.querySelectorAll(".proyectos")
-const numerosDePaginas = document.querySelectorAll('.numeros')
-
 numerosDePaginas.forEach((item, index) => {
     item.addEventListener("click", () => {
         proyectos[0].style.transform = `translateY(${-69 * index}vh)`
@@ -103,7 +85,11 @@ numerosDePaginas.forEach((item, index) => {
         inferior.classList.remove('info')
         inferior.classList.add('proyecto')
 
-        document.querySelector('.numerosDePaginas .activo').classList.remove('activo')
+        document.querySelector('.numerosDePaginas .activo').classList.remove('activo');
         item.classList.toggle('activo')
+
+        document.querySelector('.listas .I').classList.remove('activo')
+        document.querySelector('.listas .L').classList.remove('activo')
+        document.querySelector('.listas .P').classList.add('activo')
     })
 })
