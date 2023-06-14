80514af062eb584695e3470cdef89e3f21a5d89f
stvnmc <stvnmc123@gmail.com>
2023-06-13 22:04:59 -0600

    Mejorando el codigo JS N:1

--- script.js
+++ script.js
@@ -1,70 +1,35 @@
 const inferior = document.querySelector('.parteInferior')
 //Redes sociales
 const Deslizantes = document.querySelectorAll(".carpetaDeslizantes")
-const iconosDeMenu = document.querySelectorAll(".menuIteam")
+// const iconosDeMenu = document.querySelectorAll(".menuIteam")
 //Principal
 const principal = document.querySelectorAll('.lista')
 // lenguages y franworks
 const contenedor = document.querySelectorAll(".contenedor")
 const logosF = document.querySelectorAll('.frontal')
 // Proyectos
-const proyectos = document.querySelectorAll(".proyectos")
-const numerosDePaginas = document.querySelectorAll('.numeros')
-
+const Proyectoddds = document.querySelectorAll('.numerosDePaginas')
 
 //Principal
 principal.forEach((item, index) => {
     item.addEventListener("click", () => {
         if (index === 0) {
-            inferior.classList.add('arriba')
-            inferior.classList.remove('info')
-            inferior.classList.remove('proyecto')
+            abrirInformacion(iconosDeMenu)
         } else if (index === 1) {
-            inferior.classList.remove('proyecto')
-            inferior.classList.remove('arriba')
-            inferior.classList.add('info')
+            abrirInformacion(logosF)
         } else {
-            inferior.classList.add('proyecto')
-            inferior.classList.remove('arriba')
-            inferior.classList.remove('info')
+            abrirInformacion(Proyectoddds)
         }
         document.querySelector('.listas .activo').classList.remove('activo')
         item.classList.toggle('activo')
     })
 })
-
 //Redes sociales
-iconosDeMenu.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        Deslizantes[0].style.transform = `translateY(${-124 * index}%)`
-        Deslizantes[1].style.transform = `translateY(${-123 * index}%)`
-        Deslizantes[2].style.transform = `translateY(${-122 * index}%)`
-        Deslizantes[3].style.transform = `translateY(${-122 * index}%)`
-
-        inferior.classList.remove('proyecto')
-        inferior.classList.remove('info')
-        inferior.classList.add('arriba')
-
-        document.querySelector('.iconosDeMenu .activo').classList.remove('activo')
-        item.classList.toggle('activo')
-
-        document.querySelector('.listas .I').classList.add('activo')
-        document.querySelector('.listas .L').classList.remove('activo')
-        document.querySelector('.listas .P').classList.remove('activo')
-    })
-})
 
 // lenguages y franworks
 logosF.forEach((item, index) => {
     item.addEventListener("click", () => {
-        contenedor[0].style.transform = `translateY(${-109 * index}%)`
-        contenedor[1].style.transform = `translateY(${-109 * index}%)`
-        contenedor[2].style.transform = `translateY(${-109 * index}%)`
-
-        inferior.classList.remove('proyecto')
-        inferior.classList.remove('arriba')
-        inferior.classList.add('info')
-
+        abrirInformacion(logosF, index)
         document.querySelector('.locosJBR .activo').classList.remove('activo')
         item.classList.toggle('activo')
 
@@ -73,23 +38,244 @@ logosF.forEach((item, index) => {
         document.querySelector('.listas .P').classList.remove('activo')
     })
 })
-
 // Proyectos
-numerosDePaginas.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        proyectos[0].style.transform = `translateY(${-69 * index}vh)`
-        proyectos[1].style.transform = `translateY(${-69 * index}vh)`
-        proyectos[2].style.transform = `translateY(${-69 * index}vh)`
+// numerosDePaginas.forEach((item, index) => {
+//     item.addEventListener("click", () => {
+//         document.querySelector('.numerosDePaginas .activo').classList.remove('activo');
+//         item.classList.toggle('activo')
 
+//         document.querySelector('.listas .I').classList.remove('activo')
+//         document.querySelector('.listas .L').classList.remove('activo')
+//         document.querySelector('.listas .P').classList.add('activo')
+//     })
+// })
+
+const infoProyectContactos = [
+    {
+        i: 'fa-brands fa-linkedin',
+        h2: 'Linkedin',
+        p: 'este es un parofo donde tengo que esplicar lo que e visto js y venderla motos Lorem ipsum dolor',
+        img: 'img/One piece, Yamato.jpg'
+    }, {
+        i: 'fa-brands fa-discord',
+        h2: 'Instagram & Discord',
+        p: 'este es un parofo donde tengo que esplicar lo que e visto js y venderla motos Lorem ipsum dolor',
+        img: 'img/One piece, Yamato.jpg'
+    }, {
+        i: 'fa-brands fa-discord',
+        h2: 'Git',
+        p: 'este es un parofo donde tengo que esplicar lo que e visto js y venderla motos Lorem ipsum dolor',
+        img: 'img/One piece, Yamato.jpg'
+    }, {
+        i: 'fa-solid fa-crop-simple',
+        h2: 'Fraworts',
+        p: 'este es un parofo donde tengo que esplicar lo que e visto js y venderla motos Lorem ipsum dolor',
+        img: 'img/One piece, Yamato.jpg'
+    }
+]
+
+const infoProyectos = [
+    {
+        nombre: 'PokeApi',
+        img: 'img/PokeApi.jpg',
+        href: 'https://github.com/stvnmc/Mini-Proyectos_JS/tree/main/PokeApi2',
+        AboutP: 'Una PokeApi mas completa con sus con la informacion del pokemon y su generaciones'
+    },
+    {
+        nombre: '2dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '3dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '4dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '5dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '6dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '7dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '8dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '8dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '8dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '8dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '5dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '5dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '5dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '5dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '5dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    },
+    {
+        nombre: '5dsd',
+        img: 'img/blurry loading.png',
+        AboutP: 'XBOX and PLAYS'
+    }
+]
+const proyectos = document.querySelectorAll(".proyectos")
+
+const hola = () => {
+    //Proyecto    
+    const infoProyect = infoProyectos.length / 6
+    for (let i = 0; i < infoProyect; i++) {
+        buttonHTML =
+            `
+            <button class="indicador">${i + 1}</button>
+            `
+        document.getElementById('indicadores').innerHTML += buttonHTML;
+
+        if (i === 0) {
+            document.querySelector('.indicador').classList.add('activo')
+        }
+    };
+    const holaaa = document.querySelectorAll('.indicador')
+    holaaa.forEach((como, index) => {
+        como.addEventListener('click', (e) => {
+            abrirInformacion(Proyectoddds)
+            document.getElementById('proyects').style.transform = `translateY(${index * -71}vh)`
+            document.querySelector('.numerosDePaginas .activo').classList.remove('activo')
+            e.target.classList.add('activo');
+        });
+    });
+    infoProyectos.forEach(hola => {
+        generationsHTML = `
+        <figure class="contenedores">
+        <img src="${hola.img}">
+        <div class="proyectos-informacion">
+            <h2>${hola.nombre}</h2>
+            <div>
+            <a href="${hola.href}">
+            <i class="fa-brands fa-github"></i>
+            </a>
+            </div>
+            <div>
+                <p>${hola.AboutP}</p>
+            </div>
+        </div>
+    </figure>
+        `
+        proyects.innerHTML += generationsHTML;
+    });
+
+
+    // Informacion  
+    infoProyectContactos.forEach(info => {
+        buttonContactoHTML =
+            `
+            <div class="menuIteam">
+                <i class="${info.i}"></i>
+            </div>
+            `
+        document.getElementById('iconosDeMenu').innerHTML += buttonContactoHTML;
+
+        infoContactoHTML =
+            `
+            <div class="carpetaDeslizantes">
+                        <div class="descricion">
+                            <h2>${info.h2}</h2>
+                            <p>${info.p}</p>
+                        </div>
+                        <div class="foto">
+                            <img src="${info.img}">
+                        </div>
+                    </div>
+            `
+        document.getElementById('informacionDeIconos').innerHTML += infoContactoHTML;
+    })
+
+    const iconosDelMenuDeContacto = document.querySelectorAll('.menuIteam')
+
+
+    iconosDelMenuDeContacto.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            abrirInformacion(iconosDeMenu, index)
+            // document.querySelector('.iconosDeMenu .activo').classList.remove('activo')
+            // item.classList.toggle('activo')
+    
+            // document.querySelector('.listas .I').classList.add('activo')
+            // document.querySelector('.listas .L').classList.remove('activo')
+            // document.querySelector('.listas .P').classList.remove('activo')
+        })
+    })
+
+
+
+}
+
+hola()
+
+
+
+
+function abrirInformacion(openSeccion, index) {
+    if (openSeccion === principal) {
+        console.log('principal');
+    } else if (openSeccion === iconosDeMenu) {
+        document.getElementById('informacionDeIconos').style.transform = `translateY(${index * -53}vh)`
+        inferior.classList.add('arriba')
+        inferior.classList.remove('info')
+        inferior.classList.remove('proyecto')
+    }
+    else if (openSeccion === logosF) {
+        document.getElementById('informacion').style.transform = `translateY(${index * -77}vh)`
+        inferior.classList.remove('arriba')
+        inferior.classList.add('info')
+        inferior.classList.remove('proyecto')
+    }
+
+    else if (openSeccion === Proyectoddds) {
         inferior.classList.remove('arriba')
         inferior.classList.remove('info')
         inferior.classList.add('proyecto')
-
-        document.querySelector('.numerosDePaginas .activo').classList.remove('activo');
-        item.classList.toggle('activo')
-
-        document.querySelector('.listas .I').classList.remove('activo')
-        document.querySelector('.listas .L').classList.remove('activo')
-        document.querySelector('.listas .P').classList.add('activo')
-    })
-})
+    }
+}
